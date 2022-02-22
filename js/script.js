bc6a9039338977d5fd5be9fe88f3c08d6bc20026
Kresna <kresna.haltec@gmail.com>
2022-02-22 22:24:09 +0700

    Update Fitur

--- js/script.js
+++ js/script.js
@@ -1,34 +1,142 @@
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    test();
+});
+
+const test = (judul = null) => {
+    var data = [];
+    for (var key in localStorage){
+        if (Array.isArray(JSON.parse(window.localStorage.getItem(key)))) {
+            data.push(JSON.parse(window.localStorage.getItem(key)));
+        }
+     }
+     data.forEach(element => {
+         element.forEach(row => {
+             if (row['judul'] == judul) {
+                 if (row['status'] == 'false') {
+                    var html;
+                    html = '<article class="book_item" id="itemContent'+row['id']+'">'
+                    html += '<h3 id="bookTitle'+row['id']+'">'+row['judul']+'</h3>'
+                    html += '<p>Penulis: <span id="textPenulis'+row['id']+'">'+row['penulis']+'</span></p>'
+                    html += '<p>Tahun: <span id="textTahun'+row['id']+'">'+row['tahun']+'</span></p>'
+                    html += '<input type="hidden" id="check'+row['id']+'" value="'+row['check']+'"></div>'
+                    html += '<div class="action">'
+                    html += '<button class="red" onclick="destroy('+row['id']+')">Hapus buku</button>'
+                    html += '<button class="green" onclick="done('+row['id']+')">Selesai dibaca</button>'
+                    html += '</div>'
+                    html += '</article>'
+                
+                    document.getElementById('incompleteBookshelfList').insertAdjacentHTML("afterend", html)
+                } else {
+                     var html;
+                     html = '<article class="book_item" id="itemContent'+row['id']+'">'
+                     html += '<h3 id="bookTitle'+row['id']+'">'+row['judul']+'</h3>'
+                     html += '<p>Penulis: <span id="textPenulis'+row['id']+'">'+row['penulis']+'</span></p>'
+                     html += '<p>Tahun: <span id="textTahun'+row['id']+'">'+row['tahun']+'</span></p>'
+                     html += '<input type="hidden" id="check'+row['id']+'" value="'+row['check']+'"></div>'
+                     html += '<div class="action">'
+                     html += '<button class="red" onclick="destroy('+row['id']+')">Hapus buku</button>'
+                     html += '<button class="green" onclick="rollback('+row['id']+')">Rollback</button>'
+                     html += '</div>'
+                     html += '</article>'
+                 
+                     document.getElementById('completeBookshelfList').insertAdjacentHTML("afterend", html)
+                 }
+             } else {
+                if (row['status'] == 'false') {
+                    var html;
+                    html = '<article class="book_item" id="itemContent'+row['id']+'">'
+                    html += '<h3 id="bookTitle'+row['id']+'">'+row['judul']+'</h3>'
+                    html += '<p>Penulis: <span id="textPenulis'+row['id']+'">'+row['penulis']+'</span></p>'
+                    html += '<p>Tahun: <span id="textTahun'+row['id']+'">'+row['tahun']+'</span></p>'
+                    html += '<input type="hidden" id="check'+row['id']+'" value="'+row['check']+'"></div>'
+                    html += '<div class="action">'
+                    html += '<button class="red" onclick="destroy('+row['id']+')">Hapus buku</button>'
+                    html += '<button class="green" onclick="done('+row['id']+')">Selesai dibaca</button>'
+                    html += '</div>'
+                    html += '</article>'
+                
+                    document.getElementById('incompleteBookshelfList').insertAdjacentHTML("afterend", html)
+                } else {
+                     var html;
+                     html = '<article class="book_item" id="itemContent'+row['id']+'">'
+                     html += '<h3 id="bookTitle'+row['id']+'">'+row['judul']+'</h3>'
+                     html += '<p>Penulis: <span id="textPenulis'+row['id']+'">'+row['penulis']+'</span></p>'
+                     html += '<p>Tahun: <span id="textTahun'+row['id']+'">'+row['tahun']+'</span></p>'
+                     html += '<input type="hidden" id="check'+row['id']+'" value="'+row['check']+'"></div>'
+                     html += '<div class="action">'
+                     html += '<button class="red" onclick="destroy('+row['id']+')">Hapus buku</button>'
+                     html += '<button class="green" onclick="rollback('+row['id']+')">Rollback</button>'
+                     html += '</div>'
+                     html += '</article>'
+                 
+                     document.getElementById('completeBookshelfList').insertAdjacentHTML("afterend", html)
+                 }
+             }
+         })
+     });
+}
+
+document.getElementById('searchBookTitle').addEventListener("keypress", function(e) {
+    if (e.key === 'Enter') {
+        document.getElementById('searchSubmit').click();
+        e.preventDefault();
+      }
+})
+
+document.getElementById('searchSubmit').addEventListener("click", function() {
+    test(document.getElementById('searchBookTitle').value);
+    
+})
+
 document.getElementById('bookSubmit').addEventListener("click", function() {
-    var judul = document.getElementById('judul')
-    var penulis = document.getElementById('penulis')
-    var tahun = document.getElementById('tahun')
-    var inputBookIsComplete = document.getElementById('inputBookIsComplete')
-    var key = new Date().getTime();
-    var html;
-    html = '<article class="book_item" id="itemContent'+key+'">'
-    html += '<h3 id="bookTitle'+key+'"></h3>'
-    html += '<p>Penulis: <span id="textPenulis'+key+'"></span></p>'
-    html += '<p>Tahun: <span id="textTahun'+key+'"></span></p>'
-    html += '<input type="hidden" id="check'+key+'"></div>'
-    html += '<div class="action">'
-    html += '<button class="red" onclick="destroy('+key+')">Hapus buku</button>'
-    if (inputBookIsComplete.checked) {
-        html += '<button class="green" onclick="rollback('+key+')">Rollback</button>'
-    } else {
-        html += '<button class="green" onclick="done('+key+')">Selesai dibaca</button>'
+    var judul = document.getElementById('judul');
+    var penulis = document.getElementById('penulis');
+    var tahun = document.getElementById('tahun');
+    if (judul.value == '') {
+        alert("Judul harus di isi");
+        return false;
     }
-    html += '</div>'
-    html += '</article>'
+    if (penulis.value == '') {
+        alert("Penulis harus di isi");
+        return false;
+    }
+    if (tahun.value == '') {
+        alert("Tahun harus di isi");
+        return false;
+    }
+    var inputBookIsComplete = document.getElementById('inputBookIsComplete');
+    var key = new Date().getTime();
+    var array = [];
+    var html;
+    html = '<article class="book_item" id="itemContent'+key+'">';
+    html += '<h3 id="bookTitle'+key+'"></h3>';
+    html += '<p>Penulis: <span id="textPenulis'+key+'"></span></p>';
+    html += '<p>Tahun: <span id="textTahun'+key+'"></span></p>';
+    html += '<input type="hidden" id="check'+key+'"></div>';
+    html += '<div class="action">';
+    html += '<button class="red" onclick="destroy('+key+')">Hapus buku</button>';
     if (inputBookIsComplete.checked) {
-        var array = [];
-        document.getElementById('completeBookshelfList').insertAdjacentHTML("afterend", html)
+        html += '<button class="green" onclick="rollback('+key+')">Rollback</button>';
+    } else {
+        html += '<button class="green" onclick="done('+key+')">Selesai dibaca</button>';
+    }
+    html += '</div>';
+    html += '</article>';
+    if (inputBookIsComplete.checked) {
+        document.getElementById('completeBookshelfList').insertAdjacentHTML("afterend", html);
         document.getElementById('completeBookshelfList').style.display = 'block';
         document.getElementById('bookTitle'+key).innerHTML = judul.value;
         document.getElementById('textPenulis'+key).innerHTML = penulis.value;
         document.getElementById('textTahun'+key).innerHTML = tahun.value;
         document.getElementById('check'+key).value = inputBookIsComplete.checked;
         array.push({
-            'judul': judul.value, 'penulis': penulis.value, 'tahun': tahun.value
+            'id': key,
+            'judul': judul.value,
+            'penulis': penulis.value,
+            'tahun': tahun.value,
+            'check': inputBookIsComplete.checked,
+            'status': 'done'
         })
         window.localStorage.setItem(key, JSON.stringify(array))
         reset()
@@ -39,6 +147,15 @@ document.getElementById('bookSubmit').addEventListener("click", function() {
         document.getElementById('textPenulis'+key).innerHTML = penulis.value;
         document.getElementById('textTahun'+key).innerHTML = tahun.value;
         document.getElementById('check'+key).value = inputBookIsComplete.checked;
+        array.push({
+            'id': key,
+            'judul': judul.value,
+            'penulis': penulis.value,
+            'tahun': tahun.value,
+            'check': inputBookIsComplete.checked,
+            'status': 'false'
+        })
+        window.localStorage.setItem(key, JSON.stringify(array))
         reset();
     }
 })
@@ -47,6 +164,7 @@ document.getElementById('bookSubmit').addEventListener("click", function() {
     document.getElementById('judul').value = '';
     document.getElementById('penulis').value = '';
     document.getElementById('tahun').value = '';
+    document.getElementById('inputBookIsComplete').checked = false;
 }
 
 function done(id) {
@@ -56,7 +174,12 @@ function done(id) {
     var tahun = document.getElementById('textTahun'+id);
     var check = document.getElementById('check'+id).checked = true;
     array.push({
-        'judul': judul.innerHTML, 'penulis': penulis.innerHTML, 'tahun': tahun.innerHTML, 'check': check
+        'id': id,
+        'judul': judul.innerHTML,
+        'penulis': penulis.innerHTML,
+        'tahun': tahun.innerHTML,
+        'check': check,
+        'status': 'done'
     })
     window.localStorage.setItem(id, JSON.stringify(array))
     var html;
@@ -82,7 +205,12 @@ function rollback(id) {
     var tahun = document.getElementById('textTahun'+id);
     var check = document.getElementById('check'+id).checked = true;
     array.push({
-        'judul': judul.innerHTML, 'penulis': penulis.innerHTML, 'tahun': tahun.innerHTML, 'check': check
+        'id': id,
+        'judul': judul.innerHTML,
+        'penulis': penulis.innerHTML,
+        'tahun': tahun.innerHTML,
+        'check': check,
+        'status': 'false'
     })
     window.localStorage.setItem(id, JSON.stringify(array))
     var html;
